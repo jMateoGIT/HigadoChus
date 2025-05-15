4348b5563e2a87251e7905efa6ee994da97d5219
jMateoGIT <jesus94_zgz@hotmail.com>
2025-05-15 07:41:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// No se necesita JS para el efecto saltarín, pero este archivo queda preparado por si quieres añadir sonidos o interacciones después.
+console.log("El hígado está listo para el finde. ¡Cuídalo!");
